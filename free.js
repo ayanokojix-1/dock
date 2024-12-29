9e2580980274f0e85fb959d9ffe6470bb7d854a8
ayanokojix-1 <fushigurox@mailmusk.com>
2024-12-29 07:12:33 +0100

    Create free.js

---
+++ free.js
@@ -0,0 +1,18 @@
+const {  }
+async sendPoll(jid, question, options, pollType = 0) {
+  
+  const pollMessage = {
+    pollCreationMessage: {
+      name: question,
+      options: options: options.map((option) => ({ optionName: option })),
+      selectableOptionsCount: pollType, // 0 for single choice, 1 for multiple choice
+    },
+  };
+  const message = generateWAMessageFromContent(jid, proto.Message.fromObject(pollMessage), {
+    userJid: sock.user.id,
+  });
+  await sock.relayMessage(jid, message.message, {
+    messageId: message.key.id,
+  });
+  const option = ['x' ,'pp', 'Asr']
+  sendPoll(message.key.remoteJid, 'do you like cake',option)
